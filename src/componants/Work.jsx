4b324bceac121d092af9811acc2d69f06b9a25bf
Shiv sagar yadav <ssy312002@gmai.com>
2025-03-06 13:27:16 +0530

    e-comerce site

--- src/componants/Work.jsx
+++ src/componants/Work.jsx
@@ -27,7 +27,7 @@ const Work = () => {
                     <span className='text-2xl font-bold text-white tracking-wider'></span>
                     <div className='pt-8 text-center'>
                         <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                            Live
+                           <a href='https://shivsagaryadav.netlify.app/'>Live</a> 
                         </button>
                     </div>
 
